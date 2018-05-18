be37d8f2bfe5a5ed3e91715135612dd378e28744
Jacob Hayes <jacob.r.hayes@gmail.com>
2018-05-18 02:29:44 -0500

    airflow: Add runtime package installation

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -4,4 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Install additional runtime packages
+if [[ -f apt-requirements.txt ]]; then
+    apt-get update
+    xargs apt-get install -y < apt-requirements.txt
+fi
+if [[ -f requirements.txt ]]; then
+    pip3 install -r requirements.txt --no-cache-dir
+fi
+
 exec airflow "$@"
