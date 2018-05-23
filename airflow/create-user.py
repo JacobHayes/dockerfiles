9d4b9d890da1ec1d4d00211a1201f6bd72e46883
Jacob Hayes <jacob.r.hayes@gmail.com>
2018-05-23 15:24:56 -0500

    airflow: Make create-user.py script idempotent (well, skip action if user exists but don't reset passwords)

--- airflow/create-user.py
+++ airflow/create-user.py
@@ -1,17 +1,28 @@
 """ Create a airflow user
 """
 import os
+import logging
 import argparse
 
 from airflow import models, settings
 from airflow.contrib.auth.backends.password_auth import PasswordUser
 
-def create(username, email, password):
+def create(username, email, password, passive=True):
+    """ Create an airflow user. If passive=False, an error will be raised if a matching user exists.
+    """
+    session = settings.Session()
+    existing = session.query(models.User).filter(models.User.username == username).one_or_none()
+    if existing:
+        if not passive:
+            raise ValueError("User '{}' already exists!".format(username))
+        logging.info("Skipping... User '{}' already exists.".format(username))
+        return
     user = PasswordUser(models.User())
     user.username = username
     user.email = email
-    user.password = password
-    session = settings.Session()
+    # This should be: user.password = password
+    # Seems SQLA broke things in 1.2 with `hybrid_property`s. Could lower that version, or...
+    user._set_password = password
     session.add(user)
     session.commit()
     session.close()
@@ -19,17 +30,17 @@ def create(username, email, password):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        "username",
+        "--username",
         default=os.getenv("USERNAME"),
         required=not(os.getenv("USERNAME")),
     )
     parser.add_argument(
-        "email",
+        "--email",
         default=os.getenv("email"),
         required=not(os.getenv("email")),
     )
     parser.add_argument(
-        "password",
+        "--password",
         default=os.getenv("password"),
         required=not(os.getenv("password")),
     )
