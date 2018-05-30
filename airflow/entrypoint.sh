c3c11056eba40d72ba443261df77813fb1069d42
Jacob Hayes <jacob.r.hayes@gmail.com>
2018-05-30 12:10:12 -0500

    airflow: Set home dirs and extra remote logging env vars. Support apt and pip env var reqs

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -9,8 +9,15 @@ if [[ -f apt-requirements.txt ]]; then
     apt-get update
     xargs apt-get install -y < apt-requirements.txt
 fi
+if [[ -n "${APT_REQUIREMENTS:-}" ]]; then
+    apt-get update
+    apt-get install -y ${APT_REQUIREMENTS}
+fi
 if [[ -f requirements.txt ]]; then
-    pip3 install -r requirements.txt --no-cache-dir
+    pip3 install --no-cache-dir -r requirements.txt
+fi
+if [[ -n "${REQUIREMENTS:-}" ]]; then
+    pip3 install --no-cache-dir ${REQUIREMENTS}
 fi
 
 exec airflow "$@"
